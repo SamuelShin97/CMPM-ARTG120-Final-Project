047e85255c76938f5eb85aae0f82a7f2226839d9
Samuel Shin <sayshin@ucsc.edu>
2018-05-09 15:05:28 -0700

    added player movement in main and ground in main

--- game/js/prefabs/Player.js
+++ game/js/prefabs/Player.js
@@ -11,8 +11,22 @@ function Player (game, key, frame, xpos, ypos, scale)
 	this.scale.y = scale;
 
 	game.physics.enable(this);
+	this.body.gravity.y = 400; //gravity value on player (how fast player falls)
 	this.body.collideWorldBounds = true;
 }
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
+
+Ship.prototype.update = function()
+{
+	this.body.velocity.x = 0;
+	if (right == true)
+	{
+		this.body.velocity.x = 150;
+	}
+	else if (left == true)
+	{
+		this.body.velocity.x = -150;
+	}
+}
